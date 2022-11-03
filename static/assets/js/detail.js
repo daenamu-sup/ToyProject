5096a36476b016057d974f72ff00bb91f2d090f8
eunsol-an <dev.eunsol@gmail.com>
2022-11-04 01:15:49 +0900

    refactor: JS에서 HTML을 생성하지 않도록 리팩터링

--- static/assets/js/detail.js
+++ static/assets/js/detail.js
@@ -5,6 +5,7 @@ const param = window.location.search;
 const paramData = new URLSearchParams(param)
 const board_id = paramData.get('id')
 
+// 서버에서 detail 데이터 가져오기
 function detail() {
     $.ajax({
         type: 'GET',
@@ -13,43 +14,18 @@ function detail() {
         success: function (response) {
 
             // 게시글 부분
-            document.getElementById('worry-detail').innerHTML = '';
             let rows = response['worry'];
-            let board_id = rows['board_id'];
             let nickname = rows['nickname'];
-            let password = rows['password'];
             let title = rows['title'];
             let desc = rows['desc'];
             let view_count = rows['view_count'];
             let created_at = rows['created_at'];
 
-            let temp_html = `<h2 class="title border-bottom-line ps-2 pb-4 mb-4">${title}</h2>
-                              <div class="meta-top">
-                                <ul class="d-flex p-0 justify-content-between">
-                                  <li class="d-flex align-items-center mx-2"><div class="post-img"><img src="/static/assets/img/blog/comments-1.png" alt="board-img" class="rounded-circle"></div>${nickname}</li>
-                                  <li class="d-flex align-items-center mx-2">
-                                    <div class="me-3"><i class="bi bi-eye me-1"></i><a href="blog-details.html">${view_count}</a></div>
-                                    <div><i class="bi bi-clock me-1"></i><time datetime="2020-01-01">${created_at}</time></div>
-                                  </li>
-                                </ul>
-                              </div><!-- End meta top -->
-                
-                              <div class="content my-4">
-                                <p>${desc}</p>
-                              </div><!-- End post content -->
-                
-                              <div class="text-center mt-5">
-                                <div class="btn-group">
-                                  <button type="button" class="btn btn-secondary">수정</button>
-                                </div>
-                                <div class="btn-group">
-                                  <button type="button" class="btn btn-secondary">삭제</button>
-                                </div>
-                                <div class="btn-group">
-                                  <a href="/" class="btn btn-secondary">목록</a>
-                                </div>
-                              </div><!-- End button group -->`;
-            document.getElementById('worry-detail').innerHTML += temp_html;
+            document.getElementById('nickname').innerText = nickname;
+            document.getElementById('title').innerText = title;
+            document.getElementById('desc').innerText = desc;
+            document.getElementById('view_count').innerText = view_count;
+            document.getElementById('created_at').innerText = created_at;
 
             // 댓글 부분
             document.getElementById('comment').innerHTML = '';
